dadd33f3c43477047f71ead739c7bb1d668ee20a
Red Krane <rkrane99@icloud.com>
2022-04-09 01:22:29 -0400

    initial code commit

---
+++ helper/helper.go
@@ -0,0 +1,120 @@
+package helper
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+// Go starts go-routine in a safe way.
+// see protect function in https://golang.org/ref/spec#Handling_panics
+func Go(name string, g func()) {
+	go func() {
+		defer RecoverAsLogError(name)
+		g()
+	}()
+}
+
+// ResetEnv sets the environment variables to the list of variables pulled from `os.Environ()`
+func ResetEnv(environ []string) (err error) {
+	os.Clearenv()
+	for _, env := range environ {
+		keyVal := strings.SplitN(env, "=", 2)
+		err = os.Setenv(keyVal[0], keyVal[1])
+	}
+
+	return err
+}
+
+func zlogger() *zap.Logger {
+	res, err2 := zap.NewProduction()
+	ErrorHandler(err2, "zap", "logger initialization")
+	return res
+}
+
+// PanicOnError panics if error is not nil
+// It's recommended to supplement additional context information using
+// `errors.Wrapf(err, "info %s", data)`
+func PanicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// ErrorHandler is a universal handler that panics unless err is nil
+func ErrorHandler(err error, namespace, msg string) {
+	if err != nil {
+		panic(errors.Wrapf(err, "ERROR in '%s': %s\n", namespace, msg))
+	}
+}
+
+// ErrorHandlerf is a universal handler that panics unless err is nil
+func ErrorHandlerf(err error, namespace, format string, args ...interface{}) {
+	if err != nil {
+		ErrorHandler(err, namespace, fmt.Sprintf(format, args...))
+	}
+}
+
+// RecoverAsFalse recovers from panic and returns false
+func RecoverAsFalse(name string, res *bool) {
+	if err2 := recover(); err2 != nil {
+		switch err2 := err2.(type) {
+		case error:
+			zlogger().Error(name, zap.Error(err2))
+		default:
+		}
+		log.Printf("IGNORING ERROR in %s (returning false): %+v", name, err2)
+		*res = false
+	}
+}
+
+// RecoverAsLogError is a universal error recovery that logs error
+func RecoverAsLogError(label string) {
+	if err2 := recover(); err2 != nil {
+		logError(label, err2)
+	}
+}
+
+func logError(label string, err2 interface{}) {
+	switch err2 := err2.(type) {
+	case error:
+		zlogger().Error(label, zap.Error(err2))
+	default:
+	}
+	log.Printf("IGNORING ERROR in %s: %+v\n", label, err2)
+}
+
+// RecoverAsLogErrorf is a universal error recovery that logs error
+func RecoverAsLogErrorf(format string, args ...interface{}) {
+	// RecoverAsLogError(label) - this doesn't work due to limitation in `recover()`
+	// in particular, it works only in the first level of nesting.
+	// See https://golang.org/ref/spec#Handling_panics and
+	// https://stackoverflow.com/questions/49344478
+	if err2 := recover(); err2 != nil {
+		label := fmt.Sprintf(format, args...)
+		logError(label, err2)
+	}
+}
+
+// RecoverToErrorVar recovers and places the recovered error into the given variable
+func RecoverToErrorVar(name string, err *error) {
+	err2 := recover()
+	if err2 != nil {
+		log.Printf("RecoverToErrorVar2 (%s) (err=%+v), (err2: %+v\n", name, *err, err2)
+		switch err2 := err2.(type) {
+		case error:
+			err4 := errors.Wrapf(err2, "%s: Recover from panic", name)
+			*err = err4
+		case string:
+			err4 := errors.New(name + ": Recover from string-panic: " + err2)
+			*err = err4
+		default:
+			err4 := errors.New(fmt.Sprintf("%s: Recover from unknown-panic: %+v", name, err2))
+			*err = err4
+		}
+	}
+}
---
+++ helper/helper_test.go
@@ -0,0 +1,64 @@
+package helper_test
+
+import (
+	"github.com/kraneware/kore_go/helper"
+	"os"
+	"testing"
+
+	"github.com/pkg/errors"
+
+	"github.com/stretchr/testify/assert"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestGoPanicHandling(t *testing.T) {
+	var ch = make(chan bool)
+	helper.Go("anonymous_function_that_panics", func() {
+		ch <- true
+		panic(errors.New("TEST ERROR in TestGoPanicHandling. It should not fail"))
+	})
+	<-ch
+}
+
+var _ = Describe("Env Tests", func() {
+	Context("Env Test", func() {
+		It("should reset environment property", func() {
+			environ := os.Environ()
+
+			Expect(os.Setenv("TEST_KEY", "TEST_VALUE")).Should(BeNil())
+
+			Expect(os.Getenv("TEST_KEY")).Should(Equal("TEST_VALUE"))
+
+			Expect(helper.ResetEnv(environ)).Should(BeNil())
+
+			Expect(os.Getenv("TEST_KEY")).ShouldNot(Equal("TEST_VALUE"))
+		})
+	})
+})
+
+func TestPanicRecovery(t *testing.T) {
+	a, b, err2 := myFunc()
+	assert.Equal(t, 1, a)
+	assert.Equal(t, 0, b)
+	assert.NotEmpty(t, err2)
+	assert.Contains(t, err2.Error(), "CHECK")
+}
+
+func myFunc() (a, b int, err error) {
+	defer helper.RecoverToErrorVar("myFunc", &err)
+	a = 1
+	if a == 1 {
+		err2 := errors.New("TEST ERROR in myFunc. Code = CHECK")
+		helper.ErrorHandlerf(err2, "myFunc", "%s", "arg")
+	}
+	b = 1
+	return
+}
+
+func TestRecoverAsLogErrorf(t *testing.T) {
+	defer helper.RecoverAsLogErrorf("%s", "arg")
+	err2 := errors.New("TEST ERROR in TestRecoverAsLogErrorf: should not fail")
+	helper.ErrorHandlerf(err2, "TestRecoverAsLogErrorf", "%s", "arg")
+}
